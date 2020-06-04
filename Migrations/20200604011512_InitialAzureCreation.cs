e662995d67d7337d20d4c8335469cd2cc48948ce
Brad Beltman <bbeltman@dmacc.edu>
2020-06-03 22:47:35 -0500

    Initial commit

---
+++ Migrations/20200604011512_InitialAzureCreation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ch04EntityFrameworkAppBeltman.Migrations
+{
+    public partial class InitialAzureCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
