c59c3f78e95d7cb453717c2ba418a9667207ee8b
Brad Beltman <bbeltman@secureworks.com>
2020-09-22 18:39:24 -0500

    Added the Assignments App for Module 3

---
+++ AssignmentsApp/AssignmentsApp/AssignmentsApp/Controllers/Module3Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AssignmentsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentsApp.Controllers
+{
+    public class Module3Controller : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            // zero means no access level given, we still want to process this
+            if (id >= 0 && id <= 10)
+            {
+                Student student1 = new Student();
+                student1.FirstName = "Brad";
+                student1.LastName = "Beltman";
+                student1.Grade = "A";
+
+                Student student2 = new Student();
+                student2.FirstName = "George";
+                student2.LastName = "Smith";
+                student2.Grade = "B";
+
+                Student student3 = new Student();
+                student3.FirstName = "Thomas";
+                student3.LastName = "Jones";
+                student3.Grade = "C";
+
+                Student student4 = new Student();
+                student4.FirstName = "James";
+                student4.LastName = "Johnson";
+                student4.Grade = "D";
+
+                List<Student> students = new List<Student>();
+                students.Add(student1);
+                students.Add(student2);
+                students.Add(student3);
+                students.Add(student4);
+
+                var model = new StudentListViewModel
+                {
+                    Students = students,
+                    AccessLevel = id
+                };
+
+                return View(model);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ AssignmentsApp/AssignmentsApp/AssignmentsApp/Models/StudentListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentsApp.Models
+{
+    public class StudentListViewModel
+    { 
+        public List<Student> Students { get; set; }
+
+        [Range(1, 10, ErrorMessage = "The access level must be between {1} and {2}")]
+        public int AccessLevel { get; set; }
+    }
+}
