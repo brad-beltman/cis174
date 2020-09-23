054b7cb0268caab8c5dab03b3220b841b9f44cc3
Brad Beltman <bbeltman@secureworks.com>
2020-09-23 16:49:36 -0500

    Fixed some accidently moved files

---
+++ AssignmentsApp/AssignmentsApp/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentsApp.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please include a first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please include a last name")]
+        public string LastName { get; set; }
+
+        public string Grade { get; set; }
+    }
+}
