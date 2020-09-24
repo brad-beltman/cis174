8b94c4a253c4525f67f742d917b6768c7b62b9e0
Brad Beltman <bbeltman@secureworks.com>
2020-09-23 22:35:46 -0500

    Added tag helper for submit button - not working as expected

--- Module4/Module4/Controllers/HomeController.cs
+++ Module4/Module4/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Module4.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult TicketValidationSystem()
         {
             return View();
         }
---
+++ Module4/Module4/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Module4.TagHelpers
+{
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("type", "submit");
+
+            string newClasses = "btn btn-dark";
+            string oldClasses = output.Attributes["class"]?.Value?.ToString();
+            string classes = (string.IsNullOrEmpty(oldClasses)) ? newClasses : $"{oldClasses} {newClasses}";
+
+            output.Attributes.SetAttribute("class", classes);
+        }
+    }
+}
