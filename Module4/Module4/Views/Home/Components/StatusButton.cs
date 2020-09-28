6cf8572de173bb4ffa7ac8b7279b372ca7cee546
Brad Beltman <bbeltman@secureworks.com>
2020-09-28 09:50:31 -0500

    Started module 4

---
+++ Module4/Module4/Views/Home/Components/StatusButton.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Module4.Components
+{
+    public class StatusButton : ViewComponent
+    {
+        // String to hold our button style
+        public string ButtonStyle { get; set; }
+
+        public IViewComponentResult Invoke() => View(ButtonStyle);
+    }
+}
