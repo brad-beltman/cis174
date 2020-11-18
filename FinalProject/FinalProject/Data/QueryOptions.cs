ee88d724829e874150ae3454b606fca1b679daf2
Brad Beltman <bbeltman@secureworks.com>
2020-11-18 15:50:11 -0600

    Added some repository enhancements

--- FinalProject/FinalProject/Data/QueryOptions.cs
+++ FinalProject/FinalProject/Data/QueryOptions.cs
@@ -10,7 +10,8 @@ namespace FinalProject.Data
     {
         // public properties for sorting, filtering, and paging
         public Expression<Func<T, Object>> OrderBy { get; set; }
-        public Expression<Func<T, bool>> Where { get; set; }
+        public string OrderByDirection { get; set; } = "asc"; // Sort ascending by default
+        //public Expression<Func<T, bool>> Where { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
 
@@ -24,9 +25,26 @@ namespace FinalProject.Data
         // public method returns includes array
         public string[] GetIncludes() => includes ?? new string[0]; // Return empty array if no value
 
+        public WhereClauses<T> WhereClauses { get; set; }
+        public Expression<Func<T, bool>> Where
+        {
+            set
+            {
+                if (WhereClauses == null)
+                {
+                    WhereClauses = new WhereClauses<T>();
+                }
+                WhereClauses.Add(value);
+            }
+            // Added this because Visual Studio was complaining about the lack of the get accessor
+            get { return Where; }
+        }
+
         // read-only properties
-        public bool HasWhere => Where != null;
+        public bool HasWhere => WhereClauses != null;
         public bool HasOrderBy => OrderBy != null;
         public bool HasPaging => PageNumber > 0 && PageSize > 0;
     }
+
+    public class WhereClauses<T> : List<Expression<Func<T, bool>>> { }
 }
--- FinalProject/FinalProject/Data/Repositories/Repository.cs
+++ FinalProject/FinalProject/Data/Repositories/Repository.cs
@@ -17,7 +17,28 @@ namespace FinalProject.Data.Repositories
             dbset = context.Set<T>();
         }
 
+        private int? count;
+        public int Count => count ?? dbset.Count();
+
         public virtual IEnumerable<T> List(QueryOptions<T> options)
+        {
+            IQueryable<T> query = BuildQuery(options);
+            return query.ToList();
+        }
+
+        public virtual T Get(int id) => dbset.Find(id);
+        public virtual T Get(string id) => dbset.Find(id);
+        public virtual T Get(QueryOptions<T> options)
+        {
+            IQueryable<T> query = BuildQuery(options);
+            return query.FirstOrDefault();
+        }
+        public virtual void Insert(T entity) => dbset.Add(entity);
+        public virtual void Update(T entity) => dbset.Update(entity);
+        public virtual void Delete(T entity) => dbset.Remove(entity);
+        public virtual void Save() => context.SaveChanges();
+
+        private IQueryable<T> BuildQuery(QueryOptions<T> options)
         {
             IQueryable<T> query = dbset;
             foreach (string include in options.GetIncludes())
@@ -26,23 +47,28 @@ namespace FinalProject.Data.Repositories
             }
             if (options.HasWhere)
             {
-                query = query.Where(options.Where);
+                foreach (var clause in options.WhereClauses)
+                {
+                    query = query.Where(clause);
+                }
+                count = query.Count(); // Get filtered count
             }
             if (options.HasOrderBy)
             {
-                query = query.OrderBy(options.OrderBy);
+                if (options.OrderByDirection == "asc")
+                {
+                    query = query.OrderBy(options.OrderBy);
+                }
+                else
+                {
+                    query = query.OrderByDescending(options.OrderBy);
+                }
             }
             if (options.HasPaging)
             {
                 query = query.PageBy(options.PageNumber, options.PageSize);
             }
-            return query.ToList();
+            return query;
         }
-
-        public virtual T Get(int id) => dbset.Find(id);
-        public virtual void Insert(T entity) => dbset.Add(entity);
-        public virtual void Update(T entity) => dbset.Update(entity);
-        public virtual void Delete(T entity) => dbset.Remove(entity);
-        public virtual void Save() => context.SaveChanges();
     }
 }
