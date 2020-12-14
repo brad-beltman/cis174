212b6023381bcda0a91aac575554b78353463f66
Brad Beltman <bbeltman@secureworks.com>
2020-12-14 14:33:54 -0600

    Added DropDown view component

---
+++ FinalProject/FinalProject/Components/ReportTypeDropDown.cs
@@ -0,0 +1,36 @@
+﻿using FinalProject.Data;
+using FinalProject.Data.Repositories;
+using FinalProject.Models;
+using FinalProject.Models.DTOs;
+using FinalProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Components
+{
+    public class ReportTypeDropDown : ViewComponent
+    {
+        private IRepository<ReportType> _data { get; set; }
+        public ReportTypeDropDown(IRepository<ReportType> rep) => _data = rep;
+
+        public IViewComponentResult Invoke(string selectedValue)
+        {
+            var reportTypes = _data.List(new QueryOptions<ReportType>
+            {
+                OrderBy = g => g.Name
+            });
+
+            var vm = new DropDownViewModel
+            {
+                SelectedValue = selectedValue,
+                DefaultValue = ReportsGridDTO.DefaultFilter,
+                Items = reportTypes.ToDictionary(g => g.ReportTypeID.ToString(), g => g.Name)
+            };
+
+            return View("~/Views/Shared/Components/DropDown.cshtml", vm);
+        }
+    }
+}
---
+++ FinalProject/FinalProject/Models/ViewModels/DropDownViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models.ViewModels
+{
+    public class DropDownViewModel
+    {
+        public Dictionary<string, string> Items { get; set; }
+        public string SelectedValue { get; set; }
+        public string DefaultValue { get; set; }
+    }
+}
